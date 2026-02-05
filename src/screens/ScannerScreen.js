86b6c8c7768f169709a3a7476d8175d77100484d
Radoslav Otipka <radorado@centrum.sk>
2026-02-05 18:04:09 +0100

    oprava

--- src/screens/ScannerScreen.js
+++ src/screens/ScannerScreen.js
@@ -12,7 +12,7 @@ import {
 } from 'react-native';
 import { useSafeAreaInsets, SafeAreaView } from 'react-native-safe-area-context';
 import * as ImagePicker from 'expo-image-picker';
-import * as FileSystem from 'expo-file-system';
+import * as FileSystem from 'expo-file-system/legacy';
 import { manipulateAsync, SaveFormat } from 'expo-image-manipulator';
 import { CameraView, useCameraPermissions } from 'expo-camera';
 import AnalysisLoader from '../components/AnalysisLoader';
