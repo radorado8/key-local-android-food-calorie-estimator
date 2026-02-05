df19170578270adddfd2fc99145528ad6f3bff89
Radoslav Otipka <radorado@centrum.sk>
2026-02-05 12:53:44 +0100

    oprava

--- plugins/withExtractNativeLibs.js
+++ plugins/withExtractNativeLibs.js
@@ -3,8 +3,25 @@ const { withAndroidManifest } = require('expo/config-plugins');
 const withExtractNativeLibs = (config) => {
     return withAndroidManifest(config, async (config) => {
         const androidManifest = config.modResults;
+
+        // Ensure 'tools' namespace is available
+        if (!androidManifest.manifest.$['xmlns:tools']) {
+            androidManifest.manifest.$['xmlns:tools'] = 'http://schemas.android.com/tools';
+        }
+
         if (androidManifest.manifest.application && androidManifest.manifest.application[0]) {
-            androidManifest.manifest.application[0].$['android:extractNativeLibs'] = 'true';
+            const app = androidManifest.manifest.application[0];
+            app.$['android:extractNativeLibs'] = 'true';
+
+            // Add tools:replace to ensure we override any library that sets this to false
+            if (app.$['tools:replace']) {
+                const replacements = app.$['tools:replace'].split(',').map(s => s.trim());
+                if (!replacements.includes('android:extractNativeLibs')) {
+                    app.$['tools:replace'] = `${app.$['tools:replace']},android:extractNativeLibs`;
+                }
+            } else {
+                app.$['tools:replace'] = 'android:extractNativeLibs';
+            }
         }
         return config;
     });
